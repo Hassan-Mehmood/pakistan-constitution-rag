ec90a8167cc4a806dc53ac8ba86af1de8e4c27d4
Hassan-Mehmood <hassanafridi1913@gmail.com>
2024-05-05 17:20:38 +0500

    started working on the pdf loading

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 def main():
 	from langchain_community.llms import HuggingFaceEndpoint
-	from langchain.chains import LLMChain
 	from langchain_core.prompts import ChatPromptTemplate
+	import streamlit as st
+
+	from langchain_community.document_loaders import PyPDFLoader
+
+	loader = PyPDFLoader("data/data.pdf")
+	pages = loader.load_and_split()
+
+	st.title("Pakistan Constitution RAG")
+	st.subheader("Ask me questions about Pakistan Constitution.")
+
+	st.write(pages)
+
+
+
+	# repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+
+	# prompt = ChatPromptTemplate.from_messages([
+	# 	("system", "You are an assistant that will answer questions provided by the user."),
+	# 	])
+
+	# llm = HuggingFaceEndpoint(
+	# 	repo_id=repo_id, temperature=0.5
+	# 	)
 	
-	repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+	# chain = prompt | llm
 
-	prompt = ChatPromptTemplate.from_messages([
-		("system", "You are a chef who makes delicious food. Provide a detailed instruction on how to cook a dish named {dish}."),
-		])
-
-	llm = HuggingFaceEndpoint(
-		repo_id=repo_id, temperature=0.5
-		)
-	
-	chain = prompt | llm
-
-	print(chain.invoke({
-		"dish": "chicken curry",
-	}))
+	# print(chain.invoke({
+	# 	"dish": "chicken curry",
+	# }))
 
 	
 if __name__ == '__main__':
