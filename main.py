b6ba1b8176cdc0be2fb3c0b01a08b0db47fcabd3
Hassan-Mehmood <hassanafridi1913@gmail.com>
2024-05-08 21:37:49 +0500

    Working on the embeddings

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def main():
 	st.title("Pakistan Constitution RAG")
 	st.subheader("Ask me questions about Pakistan Constitution.")
 
-	loader = PyPDFLoader("data/data.pdf")
-	documents = loader.load()
+	with st.spinner('Loading documents...'):
+		loader = PyPDFLoader("data/data.pdf")
+		documents = loader.load()
 	
 	text_splitter = CharacterTextSplitter(
 		separator="\n",
@@ -17,9 +18,20 @@ def main():
 		length_function=len
 	)
 
-	texts = text_splitter.split_documents(documents)
+	with st.spinner('Splitting text chunks...'):
+		texts = text_splitter.split_documents(documents)
 
-	st.write(texts)
+	with st.spinner('Creating Embedding object...'):
+		embeddings = HuggingFaceInstructEmbeddings(
+			model_name="hkunlp/instructor-xl",
+			model_kwargs={"device": "cuda"},
+		)
+
+	with st.spinner('Embedding documents...'):
+		search = embeddings.embed_documents(texts)
+
+	st.subheader("Search:")
+
 
 if __name__ == '__main__':
 	main()
